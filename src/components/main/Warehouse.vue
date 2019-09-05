e5906a791d45b403ef2bb25633dd1df7a0894012
“liyijun123 <2510243137@qq.com>
2019-09-05 17:15:38 +0800

    finish

---
+++ src/components/main/Warehouse.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="warehouse">
+    <a-form class="wareHouseForm" layout="horizontal">
+      <a-form-item style="text-align:center">
+        <article>预约取件</article>
+      </a-form-item>
+      <a-form-item label="运单号：">
+        <a-input placeholder="运单号" v-model="billNo">
+          <a-icon slot="prefix" type="smile" style="color:rgba(0,0,0,.25)" />
+        </a-input>
+      </a-form-item>
+      <a-form-item label="取件时间" has-feedback>
+        <a-date-picker style="width: 100%" v-model="getGoodTime" format="YYYY-MM-DD HH:mm:ss"/>
+      </a-form-item>
+      <a-form-item>
+        <a-button type="primary" style="width:100%">Primary</a-button>
+      </a-form-item>
+    </a-form>
+  </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import antDesignVue from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
 
 Vue.config.productionTip = false
+Vue.use(antDesignVue)
 
 new Vue({
   router,
